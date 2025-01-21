860a951f097e826e0cdada2038cde76d44dbe767
iqra <iqrah0416@gmail.com>
2025-01-22 00:17:43 +0500

    first commit

---
+++ src/sanity/lib/client.ts
@@ -0,0 +1,11 @@
+import { createClient } from '@sanity/client';
+
+const client = createClient({
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_DATASET,
+    apiVersion: process.env.SANITY_API_VERSION,
+    useCdn: false,
+    token: process.env.SANITY_API_TOKEN,
+});
+
+export default client;
